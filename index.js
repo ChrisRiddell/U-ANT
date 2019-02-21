723ca6e538ae5060d7710c8ee9e7429191c28af9
Chris Riddell <2055566+ChrisRiddell@users.noreply.github.com>
2019-02-21 20:24:29 +1030

    WIP

--- index.js
+++ index.js
@@ -3,92 +3,176 @@
  * "u ant got no problems", A light weight tool to test latency and download speeds.
  */
 
- // TODO comment and clean up the code 
-
-// 
-const figlet = require('figlet');
-const chalk = require('chalk');
-const ora = require('ora');
-const readlineSync = require('readline-sync');
-const speedTest = require('speedtest-net');
-const ping = require('ping');
+const figlet = require('figlet')
+const chalk = require('chalk')
+// const speedTest = require('speedtest-net')
+// const ping = require('ping')
+const { prompt } = require('prompts')
+const axios = require('axios')
 
 console.log(figlet.textSync('U-ANT', {
-    horizontalLayout: 'fitted'
-}));
-
-function spinnerMessage(message, frame)
-{
-    return new ora({
-        text: message,
-        color: 'green',
-        spinner: {
-            interval: 500,
-            frames: frame
-        }
-    }).start()
-}
+  horizontalLayout: 'fitted'
+}))
 
 console.log(chalk.yellowBright(' "U Ant got no problem", A light weight tool to test latency and internet speeds! \n'))
 
-function speedtest() {
-    return new Promise(function(resolve, reject) {
-        const test = speedTest({maxTime: 5000});
-        test.on('data', data => {
-            resolve(data);
-        });
-    });
+const mainMenu = [
+  {
+    type: 'select',
+    name: 'options',
+    message: 'Pick an option',
+    choices: [
+      { title: 'Register', value: 'register' },
+      { title: 'Login', value: 'login' }
+    ],
+    initial: 1
+  }
+]
+
+const registerMenu = [
+  {
+    type: 'toggle',
+    name: 'tos',
+    message: 'Have you read the Terms of Service https://uant.riddell.id.au/tos ?',
+    initial: false,
+    active: 'yes',
+    inactive: 'no'
+  },
+  {
+    type: prev => prev === true ? 'text' : null,
+    name: 'email',
+    message: 'Email'
+  }
+]
+
+const loginMenu = [
+  {
+    type: 'password',
+    name: 'password',
+    message: 'Your Password'
+  },
+  {
+    type: prev => {
+      return axios({
+        method: 'get',
+        url: 'https://uant-api.riddell.id.au/users',
+        headers: { 'Content-Type': 'application/json', 'x-auth': prev }
+      })
+        .then(function () {
+          return 'select'
+        })
+        .catch(function () {
+          displayBlock('red', ['Password provided was not vaild.'])
+          return null
+        })
+    },
+    name: 'authoptions',
+    message: 'Pick an option',
+    choices: [
+      { title: 'Change Results URL', value: 'changePublicKey' },
+      { title: 'Change Password', value: 'changePrivateKey' },
+      { title: 'Show Results URL', value: 'showPublicKey' },
+      { title: 'Run Service', value: 'runService' }
+    ],
+    initial: 3
+  }
+]
+
+async function runUANT () {
+  const mainMenuItems = await prompt(mainMenu)
+
+  if (mainMenuItems.options === 'register') {
+    const registerMenuItems = await prompt(registerMenu)
+    if (registerMenuItems.tos === true) {
+      axios({
+        method: 'post',
+        url: 'https://uant-api.riddell.id.au/users',
+        headers: { 'Content-Type': 'application/json' },
+        data: {
+          email: registerMenuItems.email
+        }
+      }).then(function (response) {
+        displayBlock('green', ['Password', `    ${response.data.privateKey}`, 'Results URL', `    https://uant.riddell.id.au/results/${response.data.publicKey}`])
+      }).catch(function (error) {
+        if (error.response.data.error) {
+          displayBlock('red', [`${error.response.data.error.message}`])
+        }
+      })
+    }
+  }
+
+  if (mainMenuItems.options === 'login') {
+    const loginMenuItems = await prompt(loginMenu)
+
+    if (loginMenuItems.authoptions) {
+      switch (loginMenuItems.authoptions) {
+        case 'changePublicKey':
+          axios({
+            method: 'put',
+            url: 'https://uant-api.riddell.id.au/users/publickey',
+            headers: { 'Content-Type': 'application/json', 'x-auth': loginMenuItems.password },
+            data: { }
+          })
+            .then(function (response) {
+              displayBlock('green', [`New Results URL \n    https://uant.riddell.id.au/results/${response.data.publicKey}`])
+            })
+            .catch(function (error) {
+              if (error.response.data.error) {
+                displayBlock('red', [`${error.response.data.error.message}`])
+              }
+            })
+          break
+        case 'changePrivateKey':
+          axios({
+            method: 'put',
+            url: 'https://uant-api.riddell.id.au/users/privatekey',
+            headers: { 'Content-Type': 'application/json', 'x-auth': loginMenuItems.password },
+            data: { }
+          })
+            .then(function (response) {
+              displayBlock('green', [`New Password \n    ${response.data.privateKey}`])
+            })
+            .catch(function (error) {
+              if (error.response.data.error) {
+                displayBlock('red', [`${error.response.data.error.message}`])
+              }
+            })
+          break
+        case 'showPublicKey':
+          axios({
+            method: 'get',
+            url: 'https://uant-api.riddell.id.au/users/publickey',
+            headers: { 'Content-Type': 'application/json', 'x-auth': loginMenuItems.password },
+            data: { }
+          })
+            .then(function (response) {
+              displayBlock('green', [`Results URL \n    https://uant.riddell.id.au/results/${response.data.publicKey}`])
+            })
+            .catch(function (error) {
+              if (error.response.data.error) {
+                displayBlock('red', [`${error.response.data.error.message}`])
+              }
+            })
+          break
+        case 'runService':
+          displayBlock('blueBright', ['Running...', '    Results will be logged every 3 hours', '    Do not close this window'])
+
+          setInterval(function () {
+
+          }, 5000)
+
+          break
+      }
+    }
+  }
 }
 
-async function pingAddresses(urls) {
-
-    let allData = []
-
-    // fetch all the URLs in parallel
-    const pingPromises = urls.map(async url => {
-        // TODO: I need to add a catch to handle a failed ping
-        const response = await ping.promise.probe(url, {
-            timeout: 10,
-            min_reply: 25
-        });
-        return response;
-    });
-
-    // log them in sequence
-    for (const pingPromise of pingPromises) {
-        let list = await pingPromise
-        allData.push(list);
-    }
-
-    // Return a Promise so i can get to the data once all asyncs are done
-    return new Promise(function(resolve, reject) {
-        resolve(allData)
-    })
+function displayBlock (colour, messages) {
+  console.log(chalk[colour]('----------------------------------------------------------------------------'))
+  for (var m of messages) {
+    console.log(chalk[colour](m))
+  }
+  console.log(chalk[colour]('----------------------------------------------------------------------------'))
 }
 
-const spinner = spinnerMessage(' Checking latency, Please be patient!',  [' __', ' --', ' **']);
-let Downloadspinner
-
-pingAddresses(['202.142.142.142', '180.150.17.170', '1.1.1.1', 'google.com.au']).then(function(result) {
-    spinner.stop();
-
-    for (let index = 0; index < result.length; index++) {
-        console.log(chalk.blue(' ' + result[index].host))
-        console.log(chalk.green('   Minimum = ' + result[index].min + 'ms, Maximum = ' + result[index].max + 'ms, Average = ' + result[index].avg + 'ms \n'))
-    }
-
-    Downloadspinner = spinnerMessage(' Testing Speed, Please be patient!',  [' __', ' --', ' **']);
-    return speedTest({maxTime: 5000})
-
-}).then(function(result) {
-
-    result.on('data', data => {
-        Downloadspinner.stop();
-
-        console.log(chalk.blue(' Speedtest'))
-        console.log(chalk.green('   Download = '+ data.speeds.download +'Mbps, Upload = '+ data.speeds.upload +'Mbps \n'))
-
-        var userName = readlineSync.question('Close [ENTER] ');
-        console.log(' Finishing...');
-    });
-});
+runUANT()
